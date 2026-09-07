d8595a790dcfa96fbf3d250f8915d38bbc1c77ce
leobellap <leobellap@gmail.com>
2026-09-07 13:26:10 +0300

    Fixed wrong KubernetesPodOperator import path

--- dags/kubernetes_pod_operator_dag.py
+++ dags/kubernetes_pod_operator_dag.py
@@ -1,9 +1,7 @@
 from datetime import datetime
 
 from airflow.decorators import dag, task
-from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
-    KubernetesPodOperator,
-)
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 
 
 @dag(
